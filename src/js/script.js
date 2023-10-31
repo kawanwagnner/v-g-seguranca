6ec45a2b061da34d6fc2e7a0a2f5b6a55ab4431f
unknown <kawanwagnner7@gmail.com>
2023-10-31 16:45:36 -0300

    BREAKING CHANGE: add new sections, fix bugs and etc...

--- src/js/script.js
+++ src/js/script.js
@@ -137,3 +137,17 @@ btn.addEventListener("click", () => {
     link.addEventListener("click", (e) => removeMenu());
   });
 });
+
+// Pop Up
+
+$(function () {
+  $(".pop-up").hide();
+  $(".pop-up").fadeIn(1000);
+
+  $(".close-button").click(function (e) {
+    $(".pop-up").fadeOut(700);
+    $("#overlay").removeClass("blur-in");
+    $("#overlay").addClass("blur-out");
+    e.stopPropagation();
+  });
+});
